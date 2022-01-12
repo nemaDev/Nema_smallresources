cac4049711a14fa1c144504c52957d0a433065a9
Nema <petermannfelipe@gmail.com>
2022-01-12 03:32:28 -0300

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+ fx_version​ ​'​cerulean​' 
+ ​game​ ​'​gta5​' 
+  
+ ​author​ ​'ameN​' 
+ ​description​ ​'​nema-liverymenu​' 
+  
+ ​client_scripts​ { 
+ ​    ​'​@menuv/menuv.lua​'​, 
+ ​    ​'​vehicle_menu.lua​' 
+ ​}
